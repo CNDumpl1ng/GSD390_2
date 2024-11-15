8c43566db5ebc78f8672908b04d12ddd2c995a5d
CNDumpl1ng <1151479179@qq.com>
2024-11-14 21:31:07 -0600

    hw2

--- Assets/Behaviour.cs
+++ Assets/Behaviour.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
 
 public class Behaviour : MonoBehaviour
 {
-
+    //public float Speed { get; private set; } is a property that controls access to the Speed variable. It allows other classes to read the speed but not modify it, ensuring safe access.
+    //[Range(0.1f, 10f)] is an attribute applied to speedMultiplier, allowing you to adjust the multiplier within a specified range in the Unity Inspector.
+    //private static int instanceCount is a static variable shared across all instances of ExampleBehaviour. It tracks the number of active instances in the scene, useful for counting or global information.
     private static int instanceCount = 0;
     public float Speed { get; private set; } = 5f;
 
